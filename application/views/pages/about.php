459e4482690e6033f28d6681da9c6ea8568d9ca1
Paul Risk <paulrisk3@gmail.com>
2017-07-06 22:12:14 -0600

    Initial upload of web files

---
+++ application/views/pages/about.php
@@ -0,0 +1,2 @@
+<h2><?= $title ?></h2>
+<p>This is PC Repair Utah v1.0</p>
---
+++ application/views/templates/header.php
@@ -0,0 +1,27 @@
+<html>
+	<head>
+		<title>PC Repair Utah</title>
+		<link rel="stylesheet" href="https://bootswatch.com/flatly/bootstrap.min.css">
+		<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css">
+		<script src="http://cdn.ckeditor.com/4.7.1/standard/ckeditor.js"></script>
+	</head>
+	<body>
+	<nav class="navbar navbar-default">
+		<div class="container">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="<?php echo base_url(); ?>">PC Repair Utah</a>
+			</div>
+			<div id="navbar">
+				<ul class="nav navbar-nav">
+					<li><a href="<?php echo base_url(); ?>">Home</a></li>
+					<li><a href="<?php echo base_url(); ?>about">About</a></li>
+					<li><a href="<?php echo base_url(); ?>posts">Blog</a></li>
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="<?php echo base_url(); ?>posts/create">Create Post</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+
+	<div class="container">
