89468b435d942b43278038fd5ed7a72d62d10592
Paul Risk <paulrisk3@gmail.com>
2017-07-07 10:49:07 -0600

    Add sort-by-category functionality

--- application/views/posts/index.php
+++ application/views/posts/index.php
@@ -2,7 +2,7 @@
 <?php foreach($posts as $post) : ?>
 	<h3><?php echo $post['title']; ?></h3>
 	<div class="row">
-		<div class="col-md-3">
+		<div class="col-md-2">
 			<img class="post-thumb" src="<?php echo site_url(); ?>assets/images/posts/<?php echo $post['post_image']; ?>">
 		</div>
 		<div class="col-md-9">
